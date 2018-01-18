b589541bbbcf81add27f5d79926e4f495433cb89
Chandler Mayo <Chandler@ChandlerMayo.com>
2018-01-17 22:45:49 -0500

    First attempt at JSON flatten.

--- main.go
+++ main.go
@@ -9,15 +9,61 @@ import (
 	"bytes"
 	"io/ioutil"
 	"encoding/json"
+	"reflect"
+	"strconv"
 )
 
 var (
-	jsontoflatten string
 	err error
 	nestedjson map[string]interface{}
+	flattenedjson map[string]interface{} = make(map[string]interface{})
 )
 
-func GetFromURL(path string) (string, error) { // Get JSON from URL and return body as string.
+func flatten(jsontoflatten map[string]interface{}, key string, flattenedjson *map[string]interface{}) {
+	for rkey, val := range jsontoflatten {
+		fkey := key+rkey
+		valref := reflect.ValueOf(val)
+		if valref.Kind() == reflect.Interface {
+			valref = valref.Elem() // Get the value
+		}
+		switch valref.Kind() {
+			case reflect.Bool: 
+				if valref.Bool() {
+					(*flattenedjson)[fkey] = "true"
+				} else {
+					(*flattenedjson)[fkey] = "false"
+				}
+			case reflect.Int:
+				(*flattenedjson)[fkey] = fmt.Sprintf("%d", val)
+			case reflect.Float64:
+				(*flattenedjson)[fkey] = fmt.Sprintf("%f", val)
+			case reflect.String:
+				(*flattenedjson)[fkey] = val.(string)
+			case reflect.Slice:
+				for i := 0; i<len(val.([]interface{})); i++ {
+					if _, ok := val.([]string); ok {
+						(*flattenedjson)[string(i)] = val.(string)
+					} else if _, ok := val.([]int); ok {
+						(*flattenedjson)[string(i)] = val.(int)
+					} else {
+						flatten(val.([]interface{})[i].(map[string]interface{}), rkey+"["+strconv.Itoa(i)+"].", flattenedjson)
+					}
+				}
+			case reflect.Map:
+				for i := 0; i < valref.Len(); i++ {
+					flatten(val.(map[string]interface{}), rkey+".", flattenedjson)
+				}
+			default:
+				if !valref.IsValid() {
+					(*flattenedjson)[fkey] = "nil"
+				} else {
+					panic(fmt.Sprintf("Unexpected JSON value: %s", val))
+				}
+		}
+	}
+}
+
+func getfromurl(path string) (string, error) { // Get JSON from URL and return body as string.
 	response, err := http.Get(path)
 	if err != nil {
 		return "", err
@@ -36,7 +82,7 @@ func GetFromURL(path string) (string, error) { // Get JSON from URL and return b
 	}
 }
 
-func OpenFromDisk(path string) (string, error) { // Open JSON file and return body as string.
+func readfromdisk(path string) (string, error) { // Open JSON file and return body as string.
 	JSONfile, err := os.Open(path)
 	if err != nil{
 		return "", err
@@ -50,27 +96,25 @@ func OpenFromDisk(path string) (string, error) { // Open JSON file and return bo
 
 func main() {
 	if len(os.Args) > 1 {
-		jsontoflatten = os.Args[1] // Expecting a path, URL, or string.
+		jsontoflatten := os.Args[1] // Expecting a path, URL, or string.
 		if jsontoflatten != "" {
-			if strings.HasPrefix(jsontoflatten, "https://") || strings.HasPrefix(jsontoflatten, "http://") { // Check for URL prefix.
-				fmt.Println("Fetching JSON from URL.")
-				jsontoflatten, err = GetFromURL(jsontoflatten)
-				if err != nil {
-					panic(err)
-				}
-			} else if strings.HasSuffix(jsontoflatten, ".json") { // If suffixed with '.json' then assumed to be a file on disk.
-				fmt.Println("Fetching JSON from disk.")
-				jsontoflatten, err = OpenFromDisk(jsontoflatten)
-				if err != nil {
-					panic(err)
-				}
+			if strings.HasPrefix(jsontoflatten, "https://") || strings.HasPrefix(jsontoflatten, "http://") { // Get JSON from URL.
+				jsontoflatten, err = getfromurl(jsontoflatten)
+			} else if strings.HasSuffix(jsontoflatten, ".json") { // Reading JSON from disk.
+				jsontoflatten, err = readfromdisk(jsontoflatten)
 			}
-			err := json.Unmarshal([]byte(jsontoflatten), &nestedjson)
 			if err != nil {
 				panic(err)
 			}
-
-			//fmt.Println(jsontoflatten)
+			err = json.Unmarshal([]byte(jsontoflatten), &nestedjson) 
+			if err != nil {
+				fmt.Println(errors.New("Could not unmarshal JSON. A valid path, URL, or string is required."))
+				panic(err)
+			}
+			flatten(nestedjson, "", &flattenedjson) // Flatten.
+			for k, v := range flattenedjson { // Print.
+				fmt.Println(fmt.Sprintf("%s=%s", k, v))
+			}
 			return
 	    }
 	}
